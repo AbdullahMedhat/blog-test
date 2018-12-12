dee55179a43986be0d2e4d0756bd6fa7a78b9d92
Abdullah Medhat <abdullahmedhat@trycarriage.com>
2018-12-12 16:39:47 +0200

    First commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update destroy]
+
+  def index
+    @posts = Post.paginate(page: params[:page], per_page: 10)
+                 .order(created_at: :desc)
+  end
+
+  def show; end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit; end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_url, notice: 'Post was successfully destroyed.'
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content, :category_id)
+  end
+end
---
+++ db/migrate/20181205120828_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+    	t.string   :title,        null: false
+    	t.text     :content,      null: false
+    	t.integer  :category_id,  null: false
+      t.timestamps
+    end
+		add_index :posts, :category_id
+  end
+end
